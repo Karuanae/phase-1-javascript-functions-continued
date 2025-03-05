f5c0f1857b8a7353a7dd07394c353ba53a08b64a
karuana <emilykaruanamwangi@gmail.com>
2025-03-06 00:14:16 +0300

    Submit assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+
+return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (activity = "go to the office") {
+
+    return `This Monday, I will ${activity}.`;
+ 
+  };
+ 
+function wrapAdjective(flair = ""){
+return function(type = "special") {
+return `You are ${flair} ${type}${flair}`;
+};
+
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+
+
+// Example Usage:
+const starWrapper = wrapAdjective("*");
+console.log(starWrapper("a hard worker!")); // "You are *a hard worker*!"
+
+const pipeWrapper = wrapAdjective("||");
+console.log(pipeWrapper("a dedicated programmer!")); // "You are ||a dedicated programmer||!"
